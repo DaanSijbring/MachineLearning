4869ae001c5743d6afe08bdfa860b624f26f0424
DaanSijbring <daan.sijbring@gmail.com>
2018-12-21 14:58:00 +0100

    Update dataPreparation.py
    A function that reads in all data and processes it. Still needs to save tho

--- dataPreparation.py
+++ dataPreparation.py
@@ -3,9 +3,14 @@ import pandas as ps
 import re
 from collections import Counter 
 from nltk.corpus import stopwords
+from nltk.stem.porter import *
+import nltk
 from operator import itemgetter
 import matplotlib.pyplot as plt
 import os
+import string
+import time 
+from tqdm import tqdm
 #%%
 os.chdir('C:\\Users\\daan_\\GitHub\\MachineLearning\\')
 #%%
@@ -13,36 +18,43 @@ os.chdir('C:\\Users\\daan_\\GitHub\\MachineLearning\\')
 testData = ps.read_csv('drugsCom_raw\\drugsComTest_raw.tsv', sep="\t")
 trainData = ps.read_csv('drugsCom_raw\\drugsComTrain_raw.tsv', sep="\t")
 #%%
-#transform text to lower case + remove special characters
-def tokenize(text):
-    text = text.lower()
-    tokens = re.sub('[^a-z]', ' ', text).split()
-    return tokens
-#%%    
-
-def tokenizeDataSet(data):
-    tokenized = []
-    for i in range(len(data['review'])):
-        tokenized.append(tokenize(data['review'][i]))
+# Function 'negate' copied from Thomascode/negation.py
+def negate(words):
+    prev = []
+    result = []
+    neg = ["not", "n't", "no"]
+    for word in words:
+        if word not in neg:
+            if prev in neg:
+                result.append('not_' + word)
+            else:
+                result.append(word)
+        prev = word
+    return(result)
+#%%
+def processData(data):
+    import tqdm
+    filteredData = data
+    stemmer = PorterStemmer()
+    stopWords = set(stopwords.words('english'))
+    #process each review individually
+    for i in tqdm.trange(len(data)):
+        #Replace ' hex code with \'
+        rev = re.sub('&#039;',"'" ,filteredData.review[i])
+        #Remove tokens not needed
+        rev = re.sub(r'[^a-zA-Z \']+', '', rev)
+        #Translate to lower case
+        rev = rev.lower()
+        #Tokenize using NLTK's tokenizer
+        rev = nltk.word_tokenize(rev)
+        #Stem the words
+        rev = [stemmer.stem(word) for word in rev]
+        #negate
+        rev = negate(rev)
+        #Remove stopwords
+        rev = [word for word in rev if word not in stopWords]
+        filteredData.review[i] = rev
+    return filteredData
+#%%
     
-    return tokenized
-#%%
-tokTrain = tokenizeDataSet(trainData)
-conList = [item for sublist in tokTrain for item in sublist]
-counts = Counter(conList)
-#%%
-stopWords = set(stopwords.words('english'))
-len(stopWords)
-
-wordsFiltered = []
-for w in conList:
-    if w not in stopWords:
-        wordsFiltered.append(w)
-
-res = len(wordsFiltered)/len(conList)
-#%%
-counts = Counter(wordsFiltered)
-counts = list(counts.items())
-counts.sort(key=itemgetter(1),reverse=True)
-freq = [x[1] for x in counts]
-plt.plot(freq[1:10])
+a = processData(testData)
